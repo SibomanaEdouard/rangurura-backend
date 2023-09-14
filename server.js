9a65964395a645967c276d097cb8f218e216ce9e
Edouard <edouard@kali>
2023-09-14 19:12:22 +0200

    This is  initial codes

--- server.js
+++ server.js
@@ -1,66 +1,9 @@
-// // this is the server
-// //this is to require all depences
-// const {config}=require("dotenv");
-// const http=require("http");
-// const mongo=require("mongoose");
-// const express=require("express");
-// const cors=require("cors");
-// const routes=require('./Routes/routes');
-// const bodyParser=require('body-parser');
-
-
-// //this is to make express as function
-// const app=express();
-
-// //to create server
-// const server=http.createServer(app);
-// //to use config;
-// app.use(cors());
-// config();
-
-// //connect to mongodb;
-// const con=mongo.connect(process.env.MONGO)
-// .then(()=>{
-// console.log("It is connected ");
-// })
-// .catch((error)=>{
-//     console.log("It is failed to connect to mongo",error);
-// })
-
-// //this is configuration
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.json());
-
-
-
-// //this is how to connect to the routes
-// app.use('/',routes);
-
-// //this is to set the port
-// const port=process.env.PORT || 256;
-
-// //this is error handling  api
-// app.use((err, req, res, next) => {
-//     console.error(err);
-//     res.status(500).json({ error: 'Something went wrong' });
-//   });
-
-// server.listen(port,(error)=>{
-
-//     if(error){
-//     throw error;
-//     }
-//     else{
-//         console.log("the server is running on port " + port );
-//   }
-// })
-
-
 
 //this is to require all dependencies
 const {config}=require("dotenv");
 const http=require("http");
 const mongo=require("mongoose");
+const mysql=require("mysql");
 const express=require("express");
 const cors=require("cors");
 const routes=require('./Routes/routes');
@@ -75,14 +18,23 @@ const server=http.createServer(app);
 app.use(cors());
 config();
 
-//connect to mongodb;
-const con=mongo.connect(process.env.MONGO_URL)
-.then(()=>{
-console.log("It is connected ");
-})
-.catch((error)=>{
-    console.log("It is failed to connect to mongo",error);
-})
+// Create a MySQL connection pool
+const dbConfig = {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+  };
+  
+  const con = mysql.createPool(dbConfig);
+  
+  con.getConnection((err) => {
+    if (err) {
+      console.error("Failed to connect to MySQL:", err);
+    } else {
+      console.log("Connected to MySQL database");
+    }
+  });
 
 //this is configuration
 app.use(bodyParser.urlencoded({ extended: false }));
